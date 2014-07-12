ea7b6de77a51626a911fa853b09590852a4c579f
yone64 <yoneyan64@msn.com>
2014-07-12 10:50:53 +0900

    そのまま

---
+++ Demo1/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codeplex.Reactive;
+
+namespace Demo1
+{
+    class MainViewModel
+    {
+        public ReactiveProperty<string> Input { get; private set; }
+        public ReactiveProperty<string> Output { get; private set; }
+
+        public MainViewModel()
+        {
+            this.Input = new ReactiveProperty<string>("");
+
+            this.Output = Input.ToReactiveProperty();
+        }
+    }
+}
---
+++ RoomMetro26/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoomMetro26
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                .Select(i => DateTime.Now.ToString())
+                .ObserveOnDispatcher()
+                .Subscribe(t => Text.Text = t);
+        }
+    }
+}
